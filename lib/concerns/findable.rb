c0e3b279b604c7f339ccdf9c9019a3295171613e
Victor Avila <avilavictor1570@gmail.com>
2020-08-08 02:33:03 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,7 @@
 module Findable
-  
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  module ClassMethod
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
+    end
   end
-  
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-  
 end
