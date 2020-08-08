484885e01c8a371903046bf1bd34cb03aef8d63f
Victor Avila <avilavictor1570@gmail.com>
2020-08-08 02:39:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,14 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
